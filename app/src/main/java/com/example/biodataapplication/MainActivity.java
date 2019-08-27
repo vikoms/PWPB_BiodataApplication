db944eb79457057dd057e9660363c9d89ea6b3e8
vikoms <vikoms95@gmail.com>
2019-08-27 13:25:41 +0700

    first Commit

---
+++ app/src/main/java/com/example/biodataapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.biodataapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    CardView naufal,viko,firman;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        naufal = (CardView) findViewById(R.id.cardview_naufal);
+        naufal.setOnClickListener(this);
+
+        viko = (CardView) findViewById(R.id.cardview_viko);
+        viko.setOnClickListener(this);
+
+        firman = (CardView) findViewById(R.id.cardview_firman);
+        firman.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.cardview_naufal:
+
+
+                break;
+            case R.id.cardview_firman:
+
+
+                break;
+            case R.id.cardview_viko:
+
+
+                break;
+        }
+    }
+}
